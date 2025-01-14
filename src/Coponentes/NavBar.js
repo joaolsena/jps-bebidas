963bd868f47b329a30e5d858fc086a06c5c558fe
joaolsena <joaolsena129@gmail.com>
2025-01-14 00:45:01 -0300

    a

--- src/Coponentes/NavBar.js
+++ src/Coponentes/NavBar.js
@@ -1,7 +1,7 @@
 function navbar(){
     return(
         <div class="navbar">
-    
+    <h2>JPS BEBIDAS</h2>
     <nav>
       <a href="https://www.google.com/maps/place/Jps+bebidas/@0.0791245,-51.0639888,17z/data=!3m1!4b1!4m6!3m5!1s0x8d61e76424b1265d:0x4ff7e478521736a1!8m2!3d0.0791245!4d-51.0639888!16s%2Fg%2F11l728618p?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer external">📍Rua Vereador Júlio Pereira 178c, jardim 1</a>
       <a href="https://www.google.com/maps/place/Jps+bebidas/@0.0334335,-51.0523813,17z/data=!3m1!4b1!4m6!3m5!1s0x8d61e1000249dbd3:0x1d1d6defd893e017!8m2!3d0.0334335!4d-51.0498064!16s%2Fg%2F11w7p_wnqs?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer external">📍Av. Mendonça Júnior 12H, centro</a>
